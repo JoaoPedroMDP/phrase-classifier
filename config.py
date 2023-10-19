01ee669c4e9a1c9c1471fd86d30bf1d2ddf7085e
joao <joao.martins@pop-pr.rnp.br>
2023-10-19 20:22:46 -0300

    [change] Pronto. Testei vários modelos, MLP foi o com maior resultado (90%)

---
+++ config.py
@@ -0,0 +1,2 @@
+MODEL_FILENAME = "model.pckl"
+SPLIT_INTO_TRAIN_TEST = True
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #  coding: utf-8
-import datetime
-import os
+import argparse
+import csv
+import pickle
 
 import pandas
 import json
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+from config import MODEL_FILENAME, SPLIT_INTO_TRAIN_TEST
 
 
 def pega_vetores():
@@ -23,8 +32,14 @@ def formata_frases():
             linha.append(float(item))
         frases_formatadas.append(linha)
 
-    df = pandas.DataFrame(frases_formatadas)
-    df.to_csv("dados_formatados/frases_vetorizadas.csv", index=False, header=False)
+    if SPLIT_INTO_TRAIN_TEST:
+        train = frases_formatadas[:int(len(frases_formatadas) * 0.8)]
+        test = frases_formatadas[int(len(frases_formatadas) * 0.8):]
+        df = pandas.DataFrame(train)
+        df.to_csv("dados_formatados/frases_vetorizadas_train.csv", index=False, header=False)
+
+        df = pandas.DataFrame(test)
+        df.to_csv("dados_formatados/frases_vetorizadas_test.csv", index=False, header=False)
 
 
 def formata_classif():
@@ -33,8 +48,15 @@ def formata_classif():
     for classif in classif_das_frases:
         classif_formatadas.append(int(float(classif)))
 
-    df = pandas.DataFrame(classif_formatadas)
-    df.to_csv("dados_formatados/classificacao_das_frases.csv", index=False, header=False)
+
+    if SPLIT_INTO_TRAIN_TEST:
+        train = classif_formatadas[:int(len(classif_formatadas) * 0.8)]
+        test = classif_formatadas[int(len(classif_formatadas) * 0.8):]
+        df = pandas.DataFrame(train)
+        df.to_csv("dados_formatados/classificacao_das_frases_train.csv", index=False, header=False)
+
+        df = pandas.DataFrame(test)
+        df.to_csv("dados_formatados/classificacao_das_frases_test.csv", index=False, header=False)
 
 
 def formata_palavras():
@@ -61,7 +83,7 @@ def formata_vetores_palavras():
         vetores_formatados.append(linha)
 
     df = pandas.DataFrame(vetores_formatados)
-    df.to_csv(f"dados_formatados/vetores_das_palavras{datetime.datetime.now()}.csv", index=False, header=False)
+    df.to_csv(f"dados_formatados/vetores_das_palavras.csv", index=False, header=False)
 
 
 def constroi_dicionario():
@@ -90,15 +112,106 @@ def teste():
             print(f"Palavra {palavra} está indexada corretamente")
 
 
-def main():
+def sum_and_average_vectors(vect1: list, vect2: list):
+    final_vector = [None] * len(vect1)
+    for i in range(len(vect1)):
+        final_vector[i] = (vect1[i] + vect2[i]) / 2
+    
+    return final_vector
+
+
+def vectorize_phrase(frase: str):
+    frase = frase.upper()
+    print(frase)
+    dicionario: dict = json.loads(open("dados_formatados/dicionario.json").read())
+    words = frase.split(" ")
+    final_vector = []
+
+    for word in words:
+        vector = dicionario.get(word, None)
+        if not vector:
+            print(f"Não achou {word}")
+            continue
+
+        if final_vector == []:
+            final_vector = vector
+
+        final_vector = sum_and_average_vectors(final_vector, vector)
+
+    return final_vector
+
+
+def get_data(mode: str):
+    
+    with open(f"dados_formatados/frases_vetorizadas_{mode}.csv", "r") as f:
+        rows = list(csv.reader(f))
+        vectorized_phrases = []
+        for row in rows:
+            vectorized_phrases.append([float(x) for x in row])
+    
+    with open(f"dados_formatados/classificacao_das_frases_{mode}.csv", "r") as f:
+        phrases_classes = [int(x[0]) for x in list(csv.reader(f))]
+    
+    return vectorized_phrases, phrases_classes
+
+
+def look_for_trained_models():
+    filename = MODEL_FILENAME
+    try:
+        loaded_model = pickle.load(open(filename, "rb"))
+        print("Modelo carregado!!")
+        return loaded_model
+    except FileNotFoundError:
+        print("Não existem modelos prontos.")
+        return None
+
+
+def get_model(test: bool = False):
+    model = look_for_trained_models() if not test else None
+    if not model or test:
+        print("Não existe modelo pronto, treinando...")
+        print("Pegando dados de teste")
+        vectorized_phrases, phrases_classes = get_data("train")
+        
+        print("Treinando modelo")
+        # 88.84% de acurácia
+        # model: SVC = SVC().fit(vectorized_phrases, phrases_classes)
+        # 81% de acurácia
+        # model: GaussianNB = GaussianNB().fit(vectorized_phrases, phrases_classes)
+        # 85% de acurácia
+        # model: LogisticRegression = LogisticRegression().fit(vectorized_phrases, phrases_classes)
+        # 88.12% de acurácia
+        # model: RandomForestClassifier = RandomForestClassifier().fit(vectorized_phrases, phrases_classes)
+        # 83.70% de acurácia
+        # model: Perceptron = Perceptron().fit(vectorized_phrases, phrases_classes)
+        
+        # MLP
+        # 90.19% de acurácia
+        # model: MLPClassifier = MLPClassifier((3, 5)).fit(vectorized_phrases, phrases_classes)
+        # 90.38% de acurácia
+        model: MLPClassifier = MLPClassifier((3, 5, 6)).fit(vectorized_phrases, phrases_classes)
+        
+        print("Salvando modelo...")
+        pickle.dump(model, open(MODEL_FILENAME, "wb"))
+
+    return model
+
+
+def test_model(model):
+    print("Pegando dados de teste")
+    vectorized_phrases, phrases_classes = get_data("test")
+    print("Testando modelo")
+    print(model.score(vectorized_phrases, phrases_classes))
+
+def main(phrase: str, test: bool = False):
 
     # Primeiro eu formatei os arquivos pq misericórdia, .dat é de matar
-    os.makedirs("dados_formatados", exist_ok=True)
+    # os.makedirs("dados_formatados", exist_ok=True)
 
-    formata_frases()
-    formata_classif()
-    formata_palavras()
-    formata_vetores_palavras()
+    # formata_frases()
+    # formata_classif()
+    # formata_palavras()
+    # formata_vetores_palavras()
 
     # Agora quero construir um dicionario com as palavras e seus vetores
     # constroi_dicionario()
@@ -106,6 +219,31 @@ def main():
     # Farei um teste rápido pra ver se a indexação tá certinha
     # teste()
 
+    # Vetorizador de frases
+    # vector = vectorize_phrase("IMENSA IMENSO")
 
-if __name__ == '__main__':
-    main()
+    # Retorna o modelo
+    model = get_model(test)
+
+    if test:
+        print("Testando modelo")
+        test_model(model)
+        return
+
+    vectorized_phrase = vectorize_phrase(phrase)
+    if len(vectorized_phrase) == 0:
+        print(f"Não foi possível analisar a frase \"{phrase}\"")
+        return
+
+    result = model.predict([vectorized_phrase])
+    print("Boa" if result == 1 else "Ruim")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog='PhraseBadnessDetector',
+                    description='Detecta se uma frase é boa ou ruim'
+                    )
+    parser.add_argument('--phrase', required=False, help='Frase a ser analisada.')
+    parser.add_argument('--test', required=False, help='Se o modelo deve ser testado', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args.phrase, args.test)
